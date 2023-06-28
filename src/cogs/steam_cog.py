5aa19d2c4874f7c42b739fef3ab42f303b2e076f
Adrian101-hnd <69586718+Adrian101-hnd@users.noreply.github.com>
2023-06-28 16:24:05 -0600

    Player count command now working

--- src/cogs/steam_cog.py
+++ src/cogs/steam_cog.py
@@ -1,17 +1,24 @@
 import discord
 from discord.ext import commands
 import asyncio
+import os
+from dotenv import load_dotenv
+from services.steam_service import steam_fn
+load_dotenv()
 
 #This cog is for executing commands related to the steam API 👍
 class steam_cog(commands.Cog):
     def __init__(self,bot):
         self.bot = bot
+        self.steam = steam_fn(os.environ.get("STEAM_API_KEY"))
+
     
     @commands.command(name="game", aliases = ["g", "games"], help = "Gets info on a specific game")
     async def getGame(self,ctx,*args):
         query = " ".join(args)
-        print(query)
-        await ctx.send(query)
+        steam_id = self.steam.game_search(query)
+        players_n = self.steam.player_count(steam_id,query)
+        await ctx.send("The number of people playing "+ query + " is "+ str(players_n))
 
 def setup(bot):
     bot.add_cog(steam_cog(bot))
--- src/services/steam_service.py
+++ src/services/steam_service.py
@@ -5,16 +5,20 @@ import json
 from dotenv import load_dotenv
 load_dotenv()
 
-KEY = os.environ.get("STEAM_API_KEY")
-steam = Steam(KEY)
+# KEY = os.environ.get("STEAM_API_KEY")
+# steam = Steam(KEY)
 
 
-game_name = "Terraria"
+# game_name = "Terraria"
 
 class steam_fn():
 
-    def game_search(game_name):
-        dictionary = steam.apps.search_games(game_name)
+    def __init__(self, api_key):
+        print("Steam initiated")
+        self.steam = Steam(api_key)
+
+    def game_search(self,game_name):
+        dictionary = self.steam.apps.search_games(game_name)
         	
         dict_apps = dictionary["apps"]
         dict_id = dict_apps[0]
@@ -22,20 +26,19 @@ class steam_fn():
         return(str (app_id))
 
 
-    def game_details(app_id):
-        steam.apps.get_app_details(app_id)
+    def game_details(self,app_id):
+        self.steam.apps.get_app_details(app_id)
 
-    def player_count(app_id,game_name):
-        header = {"Client-ID": KEY }
+    def player_count(self,app_id,game_name):
+        header = {"Client-ID": os.environ.get("STEAM_API_KEY")}
 
         game_players_url = 'https://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1/?format=json&appid=' + app_id
         game_players = requests.get(game_players_url, headers=header)
 
-        print("Game name:"+ game_name + ", Player count: " + str(game_players.json()['response']['player_count']))
+        #print("Game name:"+ game_name + ", Player count: " + str(game_players.json()['response']['player_count']))
+        return(str(game_players.json()['response']['player_count']))
 
 
-app_id = steam_fn.game_search(game_name)
-print(steam_fn.player_count(app_id))
 
 
 
